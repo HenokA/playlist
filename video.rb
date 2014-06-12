09da8c590782cc006b51dbb44aa94f3611e69749
Henok Addis <henok4@gmail.com>
2014-06-12 10:10:23 -0400

    Broke the /sets and edit page, need to fix

--- video.rb
+++ video.rb
@@ -3,10 +3,6 @@ require 'sinatra/reloader'
 configure do
 	enable :sessions
 end
-get '/' do
-	url = makeplaylist
-	erb :index, :locals =>{:url => url}
-end
 get '/sets/new' do
 	erb :newsets
 end
@@ -16,8 +12,24 @@ get '/sets' do
 end
 post '/sets' do
 	session[:commentarr] ||= []
-	session[:commentarr].push(params[:title])
-	session[:commentarr].push(params[:comment])
+	title = "false"
+	if session[:commentarr] != []
+			a=session[:commentarr].each_slice(2)
+			videos = "false"
+			a.each do |element|	
+				if params[:name].downcase.to_s == element[0].downcase.to_s
+					title = element[0]
+					videos = element[1]
+				end
+			end
+	end
+	if title != false
+		session[:commentarr].delete(title)
+		session[:commentarr].delete(videos)
+	end
+	session[:commentarr].unshift(params[:title])
+	session[:commentarr].unshift(params[:comment])
+
 	erb :sets, :locals => { :commentarr => session[:commentarr]}
 end
 get '/sets/:name' do
@@ -45,6 +57,27 @@ get '/sets/:name/play' do
 			end
 		end
 		videos = videos.split("\n")
-			erb :index, :locals => {:title => title, :videos => videos}
+		url="http://www.youtube.com/v/" + videos[videos.length-1] + "?version=3&loop=1&playlist="+ videos[0].to_s + "," 
+ 		count = 1 
+ 		while count < videos.length-1 
+				url = url + videos[count].to_s + ", " 
+				count +=1 
+ 		end 
+ 		redirect url.to_s
+			# erb :index, :locals => {:title => url}
 
+end
+get '/sets/:name/edit' do
+		a=session[:commentarr].each_slice(2)
+		title = "false"
+		videos = "false"
+		a.each do |element|	
+			if params[:name].downcase.to_s == element[0].downcase.to_s
+				title = element[0]
+				videos = element[1]
+			else
+			end
+		end
+		videos = videos.split("\n")
+	erb :edit, :locals => {:title => title, :videos => videos}
 end
