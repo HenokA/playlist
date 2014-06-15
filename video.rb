b377a492a609ae9beaf8aa228093c6e9755f0714
AndresOrnelas <andres.ornelasvargas@yale.edu>
2014-06-15 17:17:59 -0400

    with edit file

--- video.rb
+++ video.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'enumerator'
 configure do
   enable :sessions
 end
@@ -17,19 +18,7 @@ post '/sets' do
 	session[:commentarr].push(params[:comment])
 	erb :sets, :locals => { :commentarr => session[:commentarr]}
 end
-get '/sets/:name' do
-	a=session[:commentarr].each_slice(2)
-	  title = "false"
-	  videos = "false"
-	a.each do |element|	
-		if params[:name].downcase.to_s == element[0].downcase.to_s
-			title = element[0]
-			videos = element[1]
-		else
-		end
-	end
-	erb :individual, :locals => {:title => title, :videos => videos}
-end
+
 get '/sets/:name/play' do
 	a=session[:commentarr].each_slice(2)
 		title = "false"
@@ -45,11 +34,11 @@ get '/sets/:name/play' do
 		url="http://www.youtube.com/v/" + videos[videos.length-1] + "?version=3&loop=1&playlist="+ videos[0].to_s + "," 
  		count = 1 
  		while count < videos.length-1 
-				url = url + videos[count].to_s + ", " 
+				url = url + videos[count].to_s + "," 
 				count +=1 
  		end 
- 		redirect url
-			# erb :index, :locals => {:title => url}
+ 	
+			 erb :index, :locals => {:title => url}
 
 end
 get '/sets/:name/edit' do
@@ -63,6 +52,36 @@ get '/sets/:name/edit' do
 			else
 			end
 		end
+		name = params[:name]
 		videos = videos.split("\n")
-	erb :edit, :locals => {:title => title, :videos => videos}
+	erb :edit, :locals => {:title => title, :videos => videos, :name => name}
+end
+
+get '/sets/:name' do
+	a=session[:commentarr].each_slice(2)
+	  title = "false"
+	  videos = "false"
+	a.each do |element|	
+		if params[:name].downcase.to_s == element[0].downcase.to_s
+			title = element[0]
+			videos = element[1]
+		else
+		end
+	end
+	erb :individual, :locals => {:title => title, :videos => videos}
+end
+
+put '/sets/:name' do 
+	a=session[:commentarr].each_slice(2)
+		title = "false"
+		videos = "false"
+		puts session[:commentarr]
+
+		a.each do |element|	
+			if params[:name].downcase.to_s == element[0].downcase.to_s
+				puts "hhaahahajakka"			
+			end
+		end
+	erb :individual, :locals => {:title => title, :videos => videos}
+
 end
